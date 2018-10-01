17f46f7e7412f96bf62eaa41acef2f98d523db07
Michael Xueyuan Han <hanx@g.harvard.edu>
2018-10-02 00:09:33 +0100

    add parser for streamspot data. fix typo in camflow parsers. create makefile to run them.

---
+++ camflow/prepare.py
@@ -0,0 +1,242 @@
+##############################################
+# Preprocess CamFlow W3C format provenance log
+# To be parsed into a format for Unicorn
+# data parser.
+##############################################
+
+import sys
+import json
+import logging
+import hashlib
+
+class Node:
+	def __init__(self, nid, ntype, secctx, mode, name):
+		self.nid = nid
+		self.ntype = ntype
+		self.secctx = secctx
+		self.mode = mode
+		self.name = name
+
+	def getnid(self):
+		return self.nid
+
+	def getntype(self):
+		return self.ntype
+
+	def getsecctx(self):
+		return self.secctx
+
+	def getmode(self):
+		return self.mode
+
+	def getname(self):
+		return self.name
+
+def hashgen(hashstr):
+	hasher = hashlib.md5()
+	hasher.update(hashstr)
+	return hasher.hexdigest()
+
+def nodeidgen(hashstr):
+	hasher = hashlib.md5()
+	hasher.update(hashstr)
+	return int(hasher.hexdigest()[:8], 16) # make sure it is only 32 bits
+
+def parse_nodes(json_string, node_map):
+	json_object = json.loads(json_string)
+	if "activity" in json_object:
+		activity = json_object["activity"]
+		for uid in activity:
+			if uid in node_map:
+				logging.debug("The ID of the activity node shows up more than once: %s", uid)
+			else:
+				if "prov:type" not in activity[uid]:
+					logging.debug("Skipping a problematic activity node with no 'prov:type'. ID: %s", uid)	# A node must have a type.
+				else:
+					secctx = "N/A"
+					if "cf:secctx" in activity[uid]:
+						secctx = activity[uid]["cf:secctx"]
+					secctx = hashgen(secctx)
+
+					mode = "N/A"
+					if "cf:mode" in activity[uid]:
+						mode = activity[uid]["cf:mode"]
+					mode = hashgen(mode)
+
+					name = "N/A"
+					if "cf:name" in activity[uid]:
+						name = activity[uid]["cf:name"]
+					name = hashgen(name)
+
+					node_map[uid] = Node(nodeidgen(uid), hashgen(activity[uid]["prov:type"]), secctx, mode, name)
+
+	if "entity" in json_object:
+		entity = json_object["entity"]
+		for uid in entity:
+			if uid in node_map:
+				logging.debug("The ID of the entity node shows up more than once: %s", uid)
+			else:
+				if "prov:type" not in entity[uid]:
+					logging.debug("Skipping a problematic entity node with no 'prov:type'. ID: %s", uid)
+				else:
+					secctx = "N/A"
+					if "cf:secctx" in entity[uid]:
+						secctx = entity[uid]["cf:secctx"]
+					secctx = hashgen(secctx)
+
+					mode = "N/A"
+					if "cf:mode" in entity[uid]:
+						mode = entity[uid]["cf:mode"]
+					mode = hashgen(mode)
+
+					name = "N/A"
+					if "cf:name" in entity[uid]:
+						name = entity[uid]["cf:name"]
+					name = hashgen(name)
+
+					node_map[uid] = Node(nodeidgen(uid), hashgen(entity[uid]["prov:type"]), secctx, mode, name)
+
+def parse_all_nodes(filename, node_map):
+	with open(filename) as f:
+		for line in f:
+			parse_nodes(line, node_map)
+		f.close()
+
+def parse_all_edges(inputfile, outputfile, node_map):
+	"""
+	Parse all edges with the timestamp to a file.
+	Format: <source_node_id> \t <destination_node_id> \t <hashed_source_type>:<hashed_destination_type>:<hashed_edge_type>:<edge_timestamp>
+	"""
+	output = open(outputfile, "w+")
+	with open(inputfile) as f:
+		for line in f:
+			json_object = json.loads(line)
+			
+			if "used" in json_object:
+				used = json_object["used"]
+				for uid in used:
+					from_id = used[uid]["prov:entity"]
+					to_id = used[uid]["prov:activity"]
+					if from_id not in node_map:
+						logging.debug("Skipping an edge in 'used' because we cannot find the source node. Node ID: %s", from_id)
+						continue
+					if to_id not in node_map:
+						logging.debug("Skipping an edge in 'used' because we cannot find the destination node. Node ID: %s", to_id)
+						continue
+
+					from_node = node_map[from_id]
+					from_type = hashgen(from_node.getntype() + from_node.getsecctx() + from_node.getmode() + from_node.getname())
+
+					to_node = node_map[to_id]
+					to_type = hashgen(to_node.getntype() + to_node.getsecctx() + to_node.getmode() + to_node.getname())
+					
+					edge_id = used[uid]["cf:id"]	# Can be used as timestamp
+					
+					edge_flags = "N/A"
+					if "cf:flags" in used[uid]:
+						edge_flags = used[uid]["cf:flags"]
+					edge_flags = hashgen(edge_flags)
+					edge_type = hashgen(hashgen(used[uid]["prov:type"]) + edge_flags)
+
+					output.write(str(from_node.getnid()) + '\t' + str(to_node.getnid()) + '\t' + from_type + ':' + to_type + ':' + edge_type + ':' + edge_id + '\t' + '\n')
+			
+			if "wasGeneratedBy" in json_object:
+				wasGeneratedBy = json_object["wasGeneratedBy"]
+				for uid in wasGeneratedBy:
+					from_id = wasGeneratedBy[uid]["prov:activity"]
+					to_id = wasGeneratedBy[uid]["prov:entity"]
+					if from_id not in node_map:
+						logging.debug("Skipping an edge in 'wasGeneratedBy' because we cannot find the source node. Node ID: %s", from_id)
+						continue
+					if to_id not in node_map:
+						logging.debug("Skipping an edge in 'wasGeneratedBy' because we cannot find the destination node. Node ID: %s", to_id)
+						continue
+
+					from_node = node_map[from_id]
+					from_type = hashgen(from_node.getntype() + from_node.getsecctx() + from_node.getmode() + from_node.getname())
+
+					to_node = node_map[to_id]
+					to_type = hashgen(to_node.getntype() + to_node.getsecctx() + to_node.getmode() + to_node.getname())
+
+					edge_id = wasGeneratedBy[uid]["cf:id"]
+
+					edge_flags = "N/A"
+					if "cf:flags" in wasGeneratedBy[uid]:
+						edge_flags = wasGeneratedBy[uid]["cf:flags"]
+					edge_flags = hashgen(edge_flags)
+					edge_type = hashgen(hashgen(wasGeneratedBy[uid]["prov:type"]) + edge_flags)
+					
+					output.write(str(from_node.getnid()) + '\t' + str(to_node.getnid()) + '\t' + from_type + ':' + to_type + ':' + edge_type + ':' + edge_id + '\t' + '\n')
+			
+			if "wasInformedBy" in json_object:
+				wasInformedBy = json_object["wasInformedBy"]
+				for uid in wasInformedBy:
+					from_id = wasInformedBy[uid]["prov:informant"]
+					to_id = wasInformedBy[uid]["prov:informed"]
+					if from_id not in node_map:
+						logging.debug("Skipping an edge in 'wasInformedBy' because we cannot find the source node. Node ID: %s", from_id)
+						continue
+					if to_id not in node_map:
+						logging.debug("Skipping an edge in 'wasInformedBy' because we cannot find the destination node. Node ID: %s", to_id)
+						continue
+
+					from_node = node_map[from_id]
+					from_type = hashgen(from_node.getntype() + from_node.getsecctx() + from_node.getmode() + from_node.getname())
+
+					to_node = node_map[to_id]
+					to_type = hashgen(to_node.getntype() + to_node.getsecctx() + to_node.getmode() + to_node.getname())
+
+					edge_id = wasInformedBy[uid]["cf:id"]
+
+					edge_flags = "N/A"
+					if "cf:flags" in wasInformedBy[uid]:
+						edge_flags = wasInformedBy[uid]["cf:flags"]
+					edge_flags = hashgen(edge_flags)
+					edge_type = hashgen(hashgen(wasInformedBy[uid]["prov:type"]) + edge_flags)
+
+					output.write(str(from_node.getnid()) + '\t' + str(to_node.getnid()) + '\t' + from_type + ':' + to_type + ':' + edge_type + ':' + edge_id + '\t' + '\n')
+					
+			if "wasDerivedFrom" in json_object:
+				wasDerivedFrom = json_object["wasDerivedFrom"]
+				for uid in wasDerivedFrom:
+					from_id = wasDerivedFrom[uid]["prov:usedEntity"]
+					to_id = wasDerivedFrom[uid]["prov:generatedEntity"]
+					if from_id not in node_map:
+						logging.debug("Skipping an edge in 'wasDerivedFrom' because we cannot find the source node. Node ID: %s", from_id)
+						continue
+					if to_id not in node_map:
+						logging.debug("Skipping an edge in 'wasDerivedFrom' because we cannot find the destination node. Node ID: %s", to_id)
+						continue
+
+					from_node = node_map[from_id]
+					from_type = hashgen(from_node.getntype() + from_node.getsecctx() + from_node.getmode() + from_node.getname())
+					
+					to_node = node_map[to_id]
+					to_type = hashgen(to_node.getntype() + to_node.getsecctx() + to_node.getmode() + to_node.getname())
+
+					edge_id = wasDerivedFrom[uid]["cf:id"]
+
+					edge_flags = "N/A"
+					if "cf:flags" in wasDerivedFrom[uid]:
+						edge_flags = wasDerivedFrom[uid]["cf:flags"]
+					edge_flags = hashgen(edge_flags)
+					edge_type = hashgen(hashgen(wasDerivedFrom[uid]["prov:type"]) + edge_flags)
+					
+					output.write(str(from_node.getnid()) + '\t' + str(to_node.getnid()) + '\t' + from_type + ':' + to_type + ':' + edge_type + ':' + edge_id + '\t' + '\n')
+	
+	f.close()
+	output.close()
+
+
+if __name__ == "__main__":
+	if (len(sys.argv) < 3):
+		print("""
+			Usage: python prepare.py <input_file> <output_file_with_timestamp>
+		"""
+		)
+		sys.exit(1)
+	logging.basicConfig(filename='error.log',level=logging.DEBUG)
+
+	node_map = {}
+	parse_all_nodes(sys.argv[1], node_map)
+	parse_all_edges(sys.argv[1], sys.argv[2], node_map)
---
+++ streamspot/parse.py
@@ -0,0 +1,60 @@
+import sys
+import math
+
+def read_single_graph(file_name, graph_id):
+	'''
+	Read a single graph with ID @graph_id from the file @file_name and return the list of its edges.
+	'''
+	graph = []	# Store graph edges.
+	node_id_seen = []	# Whether this node ID has been seen before.
+	cnt = 1 	# Artificial timestamps of the edges. 
+	with open(file_name) as f:
+		for line in f:
+			edge = line.strip().split("\t")
+			if edge[5] == graph_id:
+				if edge[0] in node_id_seen:	# Check if source ID has been seen before.
+					edge.append("0")
+				else:
+					edge.append("1")
+					node_id_seen.append(edge[0])
+				if edge[2] in node_id_seen:	# Check if destination ID has been seen before.
+					edge.append("0")
+				else:
+					edge.append("1")
+					node_id_seen.append(edge[2])
+				edge.append(cnt)
+				cnt = cnt + 1
+				graph.append(edge)
+	f.close()
+	return graph
+
+def print_instruction():
+	print(
+		"Usage: python parse.py <graph_id> <input_file_path> <base_graph_file_path> <stream_file_path>\n"
+		"The first 10% of the edges in the graph will be considered as the basis of the streaming graph, stored in <base_graph_file_path>.\n"
+		"The rest of the edges will be streamed in, stored in <stream_file_path>.\n")
+
+if __name__ == "__main__":
+	if (len(sys.argv) < 5):
+		print_instruction()
+		sys.exit(1)
+
+	graph = read_single_graph(sys.argv[2], sys.argv[1])
+	base_graph_size = math.ceil(len(graph) * 0.1)	# The size of base graph.
+	stream_graph_size = len(graph) - base_graph_size
+
+	base_file = open(sys.argv[3], "w")
+	stream_file = open(sys.argv[4], "w")
+
+	cnt = 0
+	for edge in graph:
+		if cnt < base_graph_size:
+			cnt = cnt + 1
+			base_file.write(str(edge[0]) + " " + str(edge[2]) + " " + edge[1] + ":" + edge[3] + ":" + edge[4] + ":" + str(edge[8]) + "\n")
+		else:
+			stream_file.write(str(edge[0]) + " " + str(edge[2]) + " " + edge[1] + ":" + edge[3] + ":" + edge[4] + ":" + edge[6] + ":" + edge[7] + ":" + str(edge[8]) + "\n")
+
+	print "[success] processing of graph ID " + sys.argv[1] + " is done. Data now can be accepted by the graph processing framework."
+	
+	base_file.close()
+	stream_file.close()
