00d821cf6e7e0e27466b6fdae59641c2d34afc57
Michael Xueyuan Han <hanx@g.harvard.edu>
2018-10-04 16:52:11 +0100

    fix a bug in the parser

--- camflow/parse.py
+++ camflow/parse.py
@@ -80,7 +80,7 @@ if __name__ == "__main__":
 	# For runtime performance eval.
 	ts_file_name = "ts_" + sys.argv[3]
 	ts_file = open(ts_file_name, "w")
-	ts = 1
+	ts = 1.0
 	edge_cnt = 0
 
 	for edge in graph:
@@ -90,9 +90,10 @@ if __name__ == "__main__":
 		else:
 			stream_file.write(str(edge[0]) + " " + str(edge[1]) + " " + edge[2] + ":" + edge[3] + ":" + edge[4] + ":" + edge[7] + ":" + edge[8] + ":" + edge[5] + ":" + edge[6] + "\n")
 			edge_cnt += 1
-			if edge[6] > ts:
+			if float(edge[6]) > ts:
 				ts += 1
 				ts_file.write(str(edge_cnt) + "\n")
+	ts_file.write(str(edge_cnt) + "\n")
 
 	print "[success] processing of " + sys.argv[1] + " is done. Data now can be accepted by the graph processing framework."
 	
