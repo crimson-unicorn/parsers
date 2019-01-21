1fb0b53f1fbb1d9a14dbc8727218aa31449d445b
Michael Xueyuan Han <hanx@g.harvard.edu>
2019-01-21 09:37:55 +0000

    add progress bar for cgendp

--- cdm/ProvParser/provparser/partool/jparse.py
+++ cdm/ProvParser/provparser/partool/jparse.py
@@ -96,7 +96,10 @@ def cgendp(parser, db, out):
 	"""
 	logging.basicConfig(filename='error.log',level=logging.DEBUG)
 
+	description = '\x1b[6;30;43m[i]\x1b[0m Progress of Generating Output'
+	pb = tqdm.tqdm(desc=description, mininterval=5.0, unit="recs", position=0)
 	for cdmrec in parser:
+		pb.update()
 		cdmrectype = cdmrec['datum'].keys()[0]
 		cdmrecval = cdmrec['datum'][cdmrectype]
 
