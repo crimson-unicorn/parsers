e7c62847137fc059238ccc838cb84080955e96fa
Michael Han <hanx@g.harvard.edu>
2020-07-26 16:25:55 +0000

    properly document streamspot parser

---
+++ streamspot/README.md
@@ -0,0 +1,71 @@
+## StreamSpot Data Parser
+This is a Python data parser for [StreamSpot data](https://github.com/sbustreamspot/sbustreamspot-data).
+You can also directly download the parsed data used in our experiments [here](https://dataverse.harvard.edu/dataset.xhtml?persistentId=doi:10.7910/DVN/83KYJY).
+The parser is designed specifically for Unicorn's graph processing framework,
+but it can be easily modified and extended.
+
+The output of this parser creates a base graph and a stream graph that streams edges onto the base graph.
+Both graphs are required for Unicorn's graph analysis pipeline.
+You have the flexibility to set the size of the base graph, however (see [Usage](#usage)).
+
+If you want to use this parser for your own graph, make sure the format of the graph aligns with StreamSpot's format
+(see [Graph Format](#graph-format)).
+
+### [Usage](#usage)
+You can either run our Python script directly or use our Makfile template that runs the parser in a virtual environment (recommended).
+Should you choose to run the script directly, you must install `tqdm` first:
+```
+pip install tqdm
+```
+You can then run:
+```
+python parser.py -h
+```
+to understand the required arguments.
+
+If you prefer using a virtual environment, make sure you have `virtualenv` installed.
+The Makefile template is located in the `example/` folder in which we include a small example to demonstrate
+how you can run the parser. Simply run:
+```
+make example
+```
+to check out the outputs. To clean up, run:
+```
+make clean
+```
+
+### [Graph Format](#graph-format)
+The input graph should have one line per edge, and each edge should look like this:
+```
+4	a	5	c	p	0
+```
+Where:
+* `4`: the node ID of the source node of the edge
+* `a`: the type of the source node
+* `5`: the node ID of the destination node of the edge
+* `c`: the type of the destination node
+* `p`: the type of the edge
+* `0`: the ID of the graph
+
+With the ID of the graph, you can in fact include multiple graphs in the same input file,
+but you must specify the ID when parsing the graph.
+
+The *base* output graph also contains one line per edge, and each edge would look like this:
+```
+4 5 a:c:p:1
+```
+The only difference from the format above, besides the format itself, is the logical timestamp (`1` in the example above)
+that is attached to each edge.
+Note that graph ID is no longer part of the edge, so the output file contains only a single graph as specified during parsing.
+
+The *stream* output graph, written to a differen file, again contains one line per edge, and each edge would look like this:
+```
+4 14 a:c:u:0:1:26
+```
+`4` and `14` are still the ID of the source and the destination node.
+`a`, `c`, and `u` are the type of the source node, the destination node, and the edge, respectively.
+`26` is the logical timestamp.
+For each edge, `0` means we have observed the same node ID previously while `1` means that this is a new node never before seen.
+There two fields are used in the later graph processing framework and may not be useful to you if you use a different framework.
+
+
--- streamspot/parse.py
+++ streamspot/parse.py
@@ -1,60 +1,72 @@
 import sys
 import math
+import tqdm
+import argparse
 
 def read_single_graph(file_name, graph_id):
-	'''
-	Read a single graph with ID @graph_id from the file @file_name and return the list of its edges.
-	'''
-	graph = []	# Store graph edges.
-	node_id_seen = []	# Whether this node ID has been seen before.
-	cnt = 1 	# Artificial timestamps of the edges. 
-	with open(file_name) as f:
-		for line in f:
-			edge = line.strip().split("\t")
-			if edge[5] == graph_id:
-				if edge[0] in node_id_seen:	# Check if source ID has been seen before.
-					edge.append("0")
-				else:
-					edge.append("1")
-					node_id_seen.append(edge[0])
-				if edge[2] in node_id_seen:	# Check if destination ID has been seen before.
-					edge.append("0")
-				else:
-					edge.append("1")
-					node_id_seen.append(edge[2])
-				edge.append(cnt)
-				cnt = cnt + 1
-				graph.append(edge)
-	f.close()
-	return graph
+    """Read a single graph with ID @graph_id from the file @file_name
+    and return the list of its edges."""
+    graph = list()                                              # list of parsed graph edges
+    node_id_seen = list()                                       # list of the node id that we have seen already
+    cnt = 1                                                     # logical timestamps of edges
 
-def print_instruction():
-	print(
-		"Usage: python parse.py <graph_id> <input_file_path> <base_graph_file_path> <stream_file_path>\n"
-		"The first 10% of the edges in the graph will be considered as the basis of the streaming graph, stored in <base_graph_file_path>.\n"
-		"The rest of the edges will be streamed in, stored in <stream_file_path>.\n")
+    desc = "\x1b[6;30;42m[STATUS]\x1b[0m Parsing StreamSpot data (graph id: {}) from {}".format(graph_id, file_name)
+    pb = tqdm.tqdm(desc=desc, mininterval=1.0, unit=" edges")
+    with open(file_name, "r") as f:
+        for line in f:
+            edge = line.strip().split("\t")
+            if edge[5] == graph_id:                             # we only parse edges that are in the graph @graph_id
+                pb.update()                                     # for progress tracking
+                if edge[0] in node_id_seen:                     # check if we have seen the source node before
+                    edge.append("0")                            # seen node is given 0 in the edge entry
+                else:
+                    edge.append("1")                            # unseen node is given 1 in the edge entry
+                    node_id_seen.append(edge[0])                # the unseen node is now seen
+                if edge[2] in node_id_seen:                     # check if we have seen the destination node before
+                    edge.append("0")
+                else:
+                    edge.append("1")
+                    node_id_seen.append(edge[2])
+                edge.append(cnt)                                # give the edge a logical timestamp
+                cnt = cnt + 1
+                graph.append(edge)
+    f.close()
+    pb.close()
+    return graph
+
 
 if __name__ == "__main__":
-	if (len(sys.argv) < 5):
-		print_instruction()
-		sys.exit(1)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-g', '--graph', help='the id of the graph to be parsed', required=True)
+    # %% is not a typo: https://thomas-cokelaer.info/blog/2014/03/python-argparse-issues-with-the-help-argument-typeerror-o-format-a-number-is-required-not-dict/
+    parser.add_argument('-s', '--size', help='the size of the base graph in absolute value (default is 10%% of the entire graph)', type=int)
+    parser.add_argument('-i', '--input', help='input StreamSpot data file path', required=True)
+    parser.add_argument('-b', '--base', help='output file path of the base graph', required=True)
+    parser.add_argument('-S', '--stream', help='output file path of the stream graph', required=True)
+    args = parser.parse_args()
 
-	graph = read_single_graph(sys.argv[2], sys.argv[1])
-	base_graph_size = math.ceil(len(graph) * 0.1)	# The size of base graph.
-	stream_graph_size = len(graph) - base_graph_size
+    graph = read_single_graph(args.input, args.graph)
+    if not args.size:
+        base_graph_size = int(math.ceil(len(graph) * 0.1))
+    else:
+        base_graph_size = args.size
+    stream_graph_size = len(graph) - base_graph_size
 
-	base_file = open(sys.argv[3], "w")
-	stream_file = open(sys.argv[4], "w")
+    base_file = open(args.base, "w")
+    stream_file = open(args.stream, "w")
 
-	cnt = 0
-	for edge in graph:
-		if cnt < base_graph_size:
-			cnt = cnt + 1
-			base_file.write(str(edge[0]) + " " + str(edge[2]) + " " + edge[1] + ":" + edge[3] + ":" + edge[4] + ":" + str(edge[8]) + "\n")
-		else:
-			stream_file.write(str(edge[0]) + " " + str(edge[2]) + " " + edge[1] + ":" + edge[3] + ":" + edge[4] + ":" + edge[6] + ":" + edge[7] + ":" + str(edge[8]) + "\n")
+    cnt = 0
+    for edge in graph:
+        if cnt < base_graph_size:
+            cnt = cnt + 1
+            base_file.write("{} {} {}:{}:{}:{}\n".format(edge[0], edge[2], edge[1], edge[3], edge[4], edge[8]))
+        else:
+            stream_file.write("{} {} {}:{}:{}:{}:{}:{}\n".format(edge[0], edge[2], edge[1], edge[3], edge[4], edge[6], edge[7], edge[8]))
 
-	print "[success] processing of graph ID " + sys.argv[1] + " is done. Data now can be accepted by the graph processing framework."
-	
-	base_file.close()
-	stream_file.close()
+    print("\x1b[6;30;42m[SUCCESS]\x1b[0m Graph {} is processed.".format(args.graph))
+    print("\x1b[6;30;42m[SUCCESS]\x1b[0m Base graph of size {} is located at {}".format(base_graph_size, args.base))
+    print("\x1b[6;30;42m[SUCCESS]\x1b[0m Stream graph of size {} is located at {}".format(stream_graph_size, args.stream))
+
+    base_file.close()
+    stream_file.close()
+
